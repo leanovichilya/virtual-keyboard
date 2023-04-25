a7b2eed2932afacb210bd6a2f50775790929f111
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-04-25 14:37:46 +0300

    refacor: delete excess import in app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 import createTextField from './components/create-text-field.js';
 import createElement from './components/create-element.js';
 import createKeyboard from './components/create-keyboard.js';
-import getValue from './components/get-value.js';
 import setTextValue from './components/set-text-value.js';
 
 document.addEventListener('DOMContentLoaded', () => {
