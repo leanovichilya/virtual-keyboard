83a347101008a5adc6809583992ea67f0f115346
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-04-26 22:04:42 +0300

    refactor: create array for saving rows

--- js/components/create-keyboard.js
+++ js/components/create-keyboard.js
@@ -1,16 +1,14 @@
 import createKeys from '../data/create-keys.js';
 import createElement from './create-element.js';
 import createKeyboardRow from './create-keyboard-row.js';
-import changeLanguage from './change-language.js';
 
-export default function createKeyboard() {
-  let lang = changeLanguage('eng');
-  const keyboard = createElement('div', 'keyboard');
+export default function createKeyboard(lang) {
+  const rows = [];
 
   createKeys().forEach((key) => {
     const row = createKeyboardRow(lang, key);
-    keyboard.append(row);
+    rows.push(row);
   });
 
-  return keyboard;
+  return rows;
 }
