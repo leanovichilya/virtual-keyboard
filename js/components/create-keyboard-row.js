fda8b451f53519f022ff79a3b3c2dfae5044b1c4
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-04-26 22:19:25 +0300

    refactor: add fucntion check-key to another file, add array buttons to save buttons row

--- js/components/create-keyboard-row.js
+++ js/components/create-keyboard-row.js
@@ -1,25 +1,19 @@
 import createElement from './create-element.js';
 import checkSymbol from './check-symbol.js';
+import checkKey from './check-key.js';
 
 export default function createKeyboardRow(lang, keys) {
-  let row = createElement('div', 'keyboard__row');
+  const buttons = [];
 
   for (let i = 0; i < keys.length; i++) {
     let keyButton = createElement('button', 'key');
     let sub = createElement('span', 'keyboard__subkey');
 
-    if (keys[i].special) {
-      keyButton.textContent = keys[i].special;
-      keyButton.dataset.special = keys[i].special;
-    } else {
-      keyButton.textContent = keys[i][lang[0]];
-      keyButton.dataset.key = keys[i][lang[0]];
-    }
-
+    checkKey(keys[i], keyButton, lang);
     checkSymbol(keyButton, sub, keys[i], lang);
 
-    row.append(keyButton);
+    buttons.push(keyButton);
   }
 
-  return row;
+  return buttons;
 }
