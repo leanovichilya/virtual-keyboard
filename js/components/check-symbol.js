22d77444763fceff76269215995490cacf3b5279
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-04-30 19:18:41 +0300

    refactor: delete redundant code

--- js/components/check-symbol.js
+++ js/components/check-symbol.js
@@ -1,65 +1,39 @@
-export default function checkSymbol(button, sub, key, lang) {
-  switch (key.special) {
-    case 'backspace':
-      button.classList.add('keyboard__key-backspace');
-      button.textContent = 'Backspace';
+export default function checkSymbol(key, keys, sub, button, lang) {
+  let classSpecialKey = '';
+
+  switch (key) {
+    case 'Backspace':
+      classSpecialKey = '-backspace';
       break;
-    case 'tab':
-      button.classList.add('keyboard__key-tab');
-      button.textContent = key.special.toUpperCase();
+    case 'Tab':
+      classSpecialKey = '-tab';
       break;
-    case 'del':
-      button.classList.add('keyboard__key-del');
-      button.textContent = key.special.toUpperCase();
+    case 'Delete':
+      classSpecialKey = '-del';
       break;
-    case 'capslock':
-      button.classList.add('keyboard__key-caps');
-      button.textContent = 'Caps Lock';
+    case 'CapsLock':
+      classSpecialKey = '-caps';
       break;
-    case 'enter':
-      button.classList.add('keyboard__key-enter');
-      button.textContent = 'Enter';
+    case 'Enter':
+      classSpecialKey = '-enter';
       break;
-    case 'shiftL':
-      button.classList.add('keyboard__key-shift-left');
-      button.textContent = 'Shift';
+    case 'ShiftLeft':
+      classSpecialKey = '-shift-left';
       break;
-    case 'shiftR':
-      button.classList.add('keyboard__key');
-      button.textContent = 'Shift';
+    case 'ControlLeft':
+      classSpecialKey = '-ctrl';
       break;
-    case 'ctrl':
-      button.classList.add('keyboard__key-ctrl');
-      button.textContent = 'Ctrl';
+    case 'ControlRight':
+      classSpecialKey = '-ctrl';
       break;
-    case 'space':
-      button.classList.add('keyboard__key-space');
-      button.textContent = ' ';
-      break;
-    case 'top':
-      button.classList.add('keyboard__key');
-      button.textContent = '△';
-      break;
-    case 'win':
-      button.classList.add('keyboard__key');
-      button.textContent = '⊞';
-      break;
-    case 'left':
-      button.classList.add('keyboard__key');
-      button.textContent = '◁';
-      break;
-    case 'down':
-      button.classList.add('keyboard__key');
-      button.textContent = '▽';
-      break;
-    case 'right':
-      button.classList.add('keyboard__key');
-      button.textContent = '▷';
+    case 'Space':
+      classSpecialKey = '-space';
       break;
     default:
-      sub.dataset.language = lang[1];
-      sub.textContent = key[lang[1]];
-      button.classList.add('keyboard__key');
+      button.textContent = keys.key || keys[lang[0]] || keys;
+      sub.textContent = keys[lang[1]];
       button.append(sub);
   }
+
+  button.classList.add(`keyboard__key${classSpecialKey}`);
 }
--- js/components/create-buttons.js
+++ js/components/create-buttons.js
@@ -1,19 +1,25 @@
 import createElement from './create-element.js';
 import checkSymbol from './check-symbol.js';
-import checkKey from './check-key.js';
 
 export default function createButtons(lang, keys) {
   const buttons = [];
 
-  for (let i = 0; i < keys.length; i++) {
-    let keyButton = createElement('button', 'key');
-    let sub = createElement('span', 'keyboard__subkey');
+  keys.forEach((rows) => {
+    for (const rowsKey in rows) {
+      let keyButton = createElement('button', 'key');
+      let sub = createElement('span', 'keyboard__subkey');
 
-    checkKey(keys[i], keyButton, lang);
-    checkSymbol(keyButton, sub, keys[i], lang);
+      keyButton.dataset.code = rowsKey;
 
-    buttons.push(keyButton);
-  }
+      if (typeof rows[rowsKey] === 'string') {
+        keyButton.textContent = rows[rowsKey];
+      }
+
+      checkSymbol(rowsKey, rows[rowsKey], sub, keyButton, lang);
+
+      buttons.push(keyButton);
+    }
+  });
 
   return buttons;
 }
