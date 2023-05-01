2fcb9b3bfc01f54e28202e0c0ad4f24f4f0e3dc1
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-05-01 14:40:01 +0300

    refactor: optimize method to change language

--- js/components/change-lang.js
+++ js/components/change-lang.js
@@ -1,11 +1,5 @@
 export default function changeLang(lang) {
-  if (lang[0] === 'eng') {
-    lang[0] = 'ru';
-    lang[1] = 'eng';
-  } else {
-    lang[0] = 'eng';
-    lang[1] = 'ru';
-  }
+  lang.unshift(lang.pop());
 
   return lang;
 }
