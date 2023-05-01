3c672123b576d32951e03fc5fda12801dde6131e
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-05-01 14:45:25 +0300

    fix: add type button

--- js/components/create-buttons.js
+++ js/components/create-buttons.js
@@ -7,16 +7,18 @@ export default function createButtons(lang, keys) {
   keys.forEach((rows) => {
     for (const rowsKey in rows) {
       let keyButton = createElement('button', 'key');
+      let span = createElement('span', 'key__name');
       let sub = createElement('span', 'keyboard__subkey');
 
       keyButton.dataset.code = rowsKey;
       keyButton.type = 'button';
 
       if (typeof rows[rowsKey] === 'string') {
-        keyButton.textContent = rows[rowsKey];
+        span.textContent = rows[rowsKey];
+        keyButton.append(span);
       }
 
-      checkSymbol(rowsKey, rows[rowsKey], sub, keyButton, lang);
+      checkSymbol(rowsKey, rows[rowsKey], sub, keyButton, lang, span);
 
       buttons.push(keyButton);
     }
